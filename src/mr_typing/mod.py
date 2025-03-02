9e3126c13614b3198dcf93285017e87bb6db5b76
Jason Livesay <runvnc@gmail.com>
2025-03-02 12:53:55 -0600

    update

--- src/mr_typing/mod.py
+++ src/mr_typing/mod.py
@@ -15,6 +15,15 @@ async def say(text, context=None):
     Return: No return value.
     """
     # create a random delay to simulate a human response
-    wait_time = random.randint(1, 5)
+    # we want the time to tend closer to 1-3 seconds with some outliers
+    # that are longer
+    wait_type = random.randint(0, 10)
+    if wait_type < 3:
+        wait_time = random.randint(2, 5)
+    else if wait_type < 9:
+        wait_time = random.randint(1, 3)
+    else:
+        wait_time = random.randint(5, 25)
+
     await asyncio.sleep(wait_time)
     return text
